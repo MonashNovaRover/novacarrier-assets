37dcd9140262b8caafe889a72b266dcd4eac365d
Matt van Wijk <mvanwijk12@gmail.com>
2025-02-22 17:39:54 +1100

    rel paths

--- bring-up/flash_novacarrier.sh
+++ bring-up/flash_novacarrier.sh
@@ -1,5 +1,11 @@
-HOST_INSTALL_DIRECTORY="/home/mvanwijk/novacarrier/test"
+#!/bin/bash
+# HOST_INSTALL_DIRECTORY="/home/mvanwijk/novacarrier/test"
+# source host_setup.sh
+export REPO_ROOT=$(dirname $(pwd))
+export HOST_INSTALL_DIRECTORY="${REPO_ROOT}/build"
 cd ${HOST_INSTALL_DIRECTORY}/Linux_for_Tegra
+
+# TODO: Check lsusb
 
 sudo ./tools/kernel_flash/l4t_initrd_flash.sh --external-device nvme0n1p1 \
 -c tools/kernel_flash//flash_l4t_t234_nvme.xml -p "-c bootloader/generic/cfg/flash_t234_qspi.xml" \
